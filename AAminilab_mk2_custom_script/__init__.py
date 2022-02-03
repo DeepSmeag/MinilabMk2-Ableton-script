b8aba408e22c6290c0834301652ebaf23cb3b443
Cosmin <43291739+DeepSmeag@users.noreply.github.com>
2022-02-03 03:20:09 +0200

    Deleted old folder
    Replaced with versions folder having both versions for Live 10 and 11

--- AAminilab_mk2_custom_script/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, print_function, unicode_literals
-from .test import test   # you import your main program during the initialisation of your script
-
-
-def create_instance(c_instance):    # this function tells live that you create a new midi remote script
-    return test(c_instance)  
